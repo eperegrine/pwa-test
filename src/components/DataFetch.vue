fb94bd77f07a214874d8fa482d9f554ecc61edf8
Emily Peregrine <peregrineemily@gmail.com>
2019-07-05 16:38:50 +0100

    Added localstorage save

---
+++ src/components/DataFetch.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <p>{{data}}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+interface SampleData {
+  name: string,
+  age: Number
+}
+
+const TEST_KEY: string = "TEST"
+
+@Component
+export default class HelloWorld extends Vue {
+  
+  private data: SampleData = {name:"", age: 0}
+
+  created() {
+    console.log(navigator);
+    
+    let a = localStorage.getItem(TEST_KEY);
+    if (a != null) {
+      console.log("FROM LS");
+      
+      this.data = JSON.parse(a);
+    } else {
+      setTimeout(() => {
+        this.data = {name:"test", age: 101};
+        localStorage.setItem(TEST_KEY, JSON.stringify(this.data));
+      }, 1000);
+    }
+  }
+
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,19 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <DataFetch />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import DataFetch from '@/components/DataFetch.vue';
 
 @Component({
   components: {
     HelloWorld,
+    DataFetch
   },
 })
 export default class Home extends Vue {}
